a74e812b713e90bcabca244dbb557e7463ae255f
aryan-michael <aryanmichael24@gmail.com>
2023-04-18 15:18:40 +0530

    basic app testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './screens/Home/Home';
+import Login from './screens/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
+
   );
 }
 
-export default App;
+export default App
---
+++ frontend/src/screens/Home/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.scss';
+import { Link, useNavigate } from 'react-router-dom';
+
+function Home() {
+    return (
+        <>
+            <div className='root-container'>
+                <Link to={`/login`}>
+                    <h1>Login</h1>
+                </Link>
+            </div>
+        </>
+    );
+}
+
+export default Home
