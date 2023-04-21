8520379be5d50add5f9230b8903ee61bcfbf793c
aryan-michael <aryanmichael24@gmail.com>
2023-04-21 14:22:28 +0530

    dashboard initialized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './screens/Home/Home';
+import Dashboard from './screens/Dashboard/Dashboard';
 import CreateAccount from './screens/CreateAccount/CreateAccount';
 import UserID from './screens/Login/UserID';
 import Password from './screens/Login/Password';
@@ -13,6 +14,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/create-account' element={<CreateAccount />} />
         <Route path='/login/user-id' element={<UserID />} />
         <Route path='/login/password' element={<Password />} />
--- frontend/src/screens/Home/Home.tsx
+++ frontend/src/screens/Home/Home.tsx
@@ -6,7 +6,11 @@ function Home() {
         <>
             <div className='root-container'>
                 <Link to={`/login/user-id`}>
-                    <h1>Login</h1>
+                    <h1>Go to Login Page</h1>
+                </Link>
+                <div />
+                <Link to={`/dashboard`}>
+                    <h1>Go to Dashboard</h1>
                 </Link>
             </div>
         </>
