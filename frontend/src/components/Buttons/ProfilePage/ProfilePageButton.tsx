01e84f223a033eb023a833565e6b5c3a0a4df12b
MihirDharaiya <mdharaiya800@gmail.com>
2023-04-28 13:53:50 +0530

    Changes in Profile

--- frontend/src/components/Buttons/ProfilePage/ProfilePageButton.tsx
+++ frontend/src/components/Buttons/ProfilePage/ProfilePageButton.tsx
@@ -1,15 +1,51 @@
-import { Box } from "@mui/material"
 
-const ProfilePageButton = () => {
-    return (
-        <div className='root'>
-            <a href="" style={{ textDecoration: "none" }}>
-                <Box sx={{ border: '2px', backgroundColor: 'pink', width: 400, height: 100 }}>
-                    <span>Your Orders</span>
-                </Box>
-            </a>
-        </div>
-    )
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "./ProfilePageButton.scss"
+
+export interface Props {
+    label1?: string;
+    label2?: string;
+    label3?: string;
+    image?: any;
+
 }
 
-export default ProfilePageButton
+const bull = (
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+        •
+    </Box>
+);
+
+export default function ProfilePageButton(props: Props) {
+    return (
+        <div className='root'>
+            <a href='' style={{ textDecoration: 'none' }}>
+                <Card sx={{ width: 300, boxShadow: '6', alignItems: 'center', justifyContent: 'center' }}>
+                    <div className='sub'>
+                        <img className='image' src={props.image} alt="image" />
+                    </div>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            {props.label1}
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            {props.label2}
+                        </Typography>
+                        <Typography variant="body2">
+                            {props.label3}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </a>
+        </div>
+    );
+}
--- frontend/src/screens/Profile/Profile.tsx
+++ frontend/src/screens/Profile/Profile.tsx
@@ -1,5 +1,7 @@
 import NavBar from "../../components/NavBar/NavBar"
 import ProfilePageButton from "../../components/Buttons/ProfilePage/ProfilePageButton"
+import image from '../../images/settings.jpg';
+import "./Profile.scss"
 
 const Profile = () => {
     return (
@@ -7,8 +9,27 @@ const Profile = () => {
             <div>
                 <NavBar />
             </div>
-            <div>
-                <ProfilePageButton />
+            <div className="box">
+                <h2>Your Account</h2>
+                <div className="box-sub">
+                    <ProfilePageButton
+                        label2="Your Orders"
+                    />
+                    <ProfilePageButton
+                        image={image}
+                        label2="Account Settings"
+                    />
+                    <ProfilePageButton
+                        label2="Your Returns"
+                    />
+                    <ProfilePageButton
+                        label2="Your Addresses"
+                    />
+                    <ProfilePageButton
+                        label2="Contact Us"
+                    />
+                </div>
+
             </div>
         </div>
     )
